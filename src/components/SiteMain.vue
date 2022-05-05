7623c5311d43cd2e9094d3491e8ce23c272bc841
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-05 15:56:31 +0200

    edit components

---
+++ src/components/SiteMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+      <CharactersList />
+  </main>
+</template>
+
+<script>
+import CharactersList from "@/components/CharactersListComponent.vue";
+export default {
+  name: "SiteMain",
+  components: {
+      CharactersList
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+//aggiungere import dei font da fontsource
+
+//importare fontawesome
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+
+const bootstrap = require('bootstrap') 
+Vue.use(bootstrap)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
